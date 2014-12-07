d9fe1c5318b3c5dbaae6e673e2945eac6e2a1d5c
NumberJuan617 <juanluisandujar10@gmail.com>
2014-12-07 15:25:21 -0500

    finishing layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'users/new'
+
   root 'main_static_pages#home'
   get 'about' => 'main_static_pages#about'
   get 'contact' => 'main_static_pages#contact'
@@ -7,6 +9,7 @@ Rails.application.routes.draw do
   get 'recipe' => 'main_static_pages#recipe'
   get 'contact' => 'main_static_pages#contact'
   get 'resources' => 'main_static_pages#resources'
+  get 'signup' => 'users#new'
 
 
 
