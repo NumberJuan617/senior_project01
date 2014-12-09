8cfa084de8246abf5a6d8c58f5b5632e6639f022
NumberJuan617 <juanluisandujar10@gmail.com>
2014-12-09 14:16:21 -0500

    added index to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+	before_save { self.email = email.downcase }#call back function that ensures the email format is lowercase or downcase 
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :fname,:lname,  presence: true, length: { maximum: 50 }
 	validates :email, presence: true, length: { maximum: 255 },  
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141209163156) do
+ActiveRecord::Schema.define(version: 20141209191145) do
 
   create_table "users", force: true do |t|
     t.string   "fname"
@@ -21,5 +21,7 @@ ActiveRecord::Schema.define(version: 20141209163156) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
