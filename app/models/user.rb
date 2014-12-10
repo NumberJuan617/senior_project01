509f3c969abf1bb9b33bfd0977ae466dde6ddece
NumberJuan617 <juanluisandujar10@gmail.com>
2014-12-09 19:41:34 -0500

    Adding user new, create, and show functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ActiveRecord::Base
-	before_save { self.email = email.downcase }#call back function that ensures the email format is lowercase or downcase 
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	#before_save { self.email = email.downcase }
 	validates :fname,:lname,  presence: true, length: { maximum: 50 }
-	validates :email, presence: true, length: { maximum: 255 },  
-				format: { with: VALID_EMAIL_REGEX }, 
-				uniqueness: { case_sensitive: false }
-	validates :age, presence: true, length: { maximum:4 }
-  	has_secure_password	
-  	validates :password, length: { minimum: 6}
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+	validates :age, presence: true, length: { maximum:3}
+	has_secure_password	
+	validates :password, length: { minimum: 6 }
 end
