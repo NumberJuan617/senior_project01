2fe3053a1881173e06e3a5f10d4cd2383c1f6c74
NumberJuan617 <juanluisandujar10@gmail.com>
2014-12-09 20:48:38 -0500

    adding session helpers and functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,21 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
-
+\
   root 'main_static_pages#home'
+  #main static pages controller routes
   get 'about' => 'main_static_pages#about'
   get 'contact' => 'main_static_pages#contact'
   get 'home' => 'main_static_pages#home'
   get 'recipe' => 'main_static_pages#recipe'
   get 'contact' => 'main_static_pages#contact'
   get 'resources' => 'main_static_pages#resources'
+  #user controller routes
   get 'signup' => 'users#new'
+  #session controller routes....login sends a GET and POST request 
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+
+  #resources utilized ith user model. 
   resources :users
 end
