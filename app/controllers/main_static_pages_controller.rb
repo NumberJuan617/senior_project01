e61857258c7fa45254f1f2cbc9921312e004dc64
NumberJuan617 <juanluisandujar10@gmail.com>
2015-03-25 23:00:15 -0400

    Added notes to static pages controller

--- app/controllers/main_static_pages_controller.rb
+++ app/controllers/main_static_pages_controller.rb
@@ -1,10 +1,10 @@
 class MainStaticPagesController < ApplicationController
   def home
-
+    #home will be its own controller consisting of View all Friends
   end
 
   def recipe
-
+    #recipe will be its own controller consisting of ViewRecipes, Edit, Delete,, 
   end
 
   def resources
